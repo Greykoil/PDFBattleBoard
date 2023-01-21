6d682a40f1dada433898091472d797374818fde3
Alex Spencer <Alex.I.J.Spencer@gmail.com>
2023-01-21 13:59:45 +0000

    Scale text when required. Add boxes vs circles Improve ability merging.

--- PDFBattleBoard/View/ChargedAbilityDrawer.cs
+++ PDFBattleBoard/View/ChargedAbilityDrawer.cs
@@ -3,6 +3,13 @@ using PdfSharpCore.Drawing;
 
 namespace PDFBattleBoard.View
 {
+    struct ChargedAbilitySplit
+    {
+        public IEnumerable<ChargedAbility> SingleLineAbilites { get; set; }
+        public IEnumerable<ChargedAbility> DoubledUpAbilies { get; set; }
+        public IEnumerable<ChargedAbility> TripledUpAbilities { get; set; }
+    }
+
     internal class ChargedAbilityDrawer
     {
 
@@ -15,28 +22,26 @@ namespace PDFBattleBoard.View
 
         public double CalculateAbilityHeight(IEnumerable<ChargedAbility> abilities)
         {
-            var singleLineAbilites = abilities.Where(x => x.Charges > 7 || x.Frequent == Frequency.Sectional && x.Charges > 2);
-
-            var doubledUpAbilites = abilities.Where(x => !singleLineAbilites.Contains(x));
+            var abilitySplit = CreateAbilityDistributions(abilities);
 
             return
                 DrawingUtils.RegionBuffer * 2 + // The buffer above and below the box
                 DrawingUtils.DefaultLineHeight + // The header line
-                (singleLineAbilites.Count() * DrawingUtils.DefaultLineHeight) + // One line per single line ability
-                Math.Ceiling((double)doubledUpAbilites.Count() / 2) * DrawingUtils.DefaultLineHeight; // One line for every 2 doubled up abilites
+                (abilitySplit.SingleLineAbilites.Count() * DrawingUtils.DefaultLineHeight) + // One line per single line ability
+                Math.Ceiling((double)abilitySplit.DoubledUpAbilies.Count() / 2) * DrawingUtils.DefaultLineHeight + // One line for every 2 doubled up abilites
+                Math.Ceiling((double)abilitySplit.TripledUpAbilities.Count() / 3) * DrawingUtils.DefaultLineHeight  // One line for every 3 trippled up abilites
+                ;
         }
 
         public void DrawChargedSkills(IEnumerable<ChargedAbility> abilities, XRect xRect)
         {
             var innerRegion = DrawingUtils.CreateRegion(xRect, abilities.First().Source.ToString());
 
-            var singleLineAbilites = abilities.Where(x => x.Charges > 7 || x.Frequent == Frequency.Sectional && x.Charges > 2);
-
-            var doubledUpAbilites = abilities.Where(x => !singleLineAbilites.Contains(x));
+            var splitAbilites = CreateAbilityDistributions(abilities);
 
             var totalWidth = innerRegion.Width;
             var currentPoint = innerRegion.TopLeft;
-            foreach (var item in singleLineAbilites)
+            foreach (var item in splitAbilites.SingleLineAbilites)
             {
                 var nameWidth = totalWidth / 4;
                 DrawingUtils.TextRectangle(item.Name, nameWidth, currentPoint);
@@ -51,9 +56,13 @@ namespace PDFBattleBoard.View
                         currentPoint.Offset(chargesWidth, 0);
                     }
                 }
-                else
+                else if (item.Frequent == Frequency.Daily)
                 {
                     DrawingUtils.CheckCircleRectangle(item.Charges, chargesWidth, currentPoint);
+                }
+                else
+                {
+                    DrawingUtils.CheckBoxRectangle(item.Charges, chargesWidth, currentPoint);
                 }
 
                 currentPoint.X = innerRegion.Left;
@@ -62,7 +71,7 @@ namespace PDFBattleBoard.View
 
             var halfWidth = totalWidth / 2;
             bool halfOffset = false;
-            foreach (var item in doubledUpAbilites)
+            foreach (var item in splitAbilites.DoubledUpAbilies)
             {
                 var nameWidth = totalWidth / 4;
                 DrawingUtils.TextRectangle(item.Name, nameWidth, currentPoint);
@@ -77,9 +86,13 @@ namespace PDFBattleBoard.View
                         currentPoint.Offset(chargesWidth, 0);
                     }
                 }
-                else
+                else if (item.Frequent == Frequency.Daily)
                 {
                     DrawingUtils.CheckCircleRectangle(item.Charges, chargesWidth, currentPoint);
+                }
+                else
+                {
+                    DrawingUtils.CheckBoxRectangle(item.Charges, chargesWidth, currentPoint);
                 }
 
                 if (halfOffset)
@@ -90,10 +103,86 @@ namespace PDFBattleBoard.View
                 }
                 else
                 {
-                    halfOffset=true;
+                    halfOffset = true;
                     currentPoint.X = innerRegion.Left + halfWidth;
                 }
             }
+
+            if (halfOffset)
+            {
+                currentPoint.X = innerRegion.Left;
+                currentPoint.Offset(0, DrawingUtils.DefaultLineHeight);
+            }
+
+            var thirdWidth = totalWidth / 3;
+            int indendationLevel = 0;
+
+            foreach (var item in splitAbilites.TripledUpAbilities)
+            {
+                var nameWidth = totalWidth / 4;
+                DrawingUtils.TextRectangle(item.Name, nameWidth, currentPoint);
+                currentPoint.Offset(nameWidth, 0);
+                var chargesWidth = thirdWidth - nameWidth;
+
+                if (item.Frequent == Frequency.Daily)
+                {
+                    DrawingUtils.CheckCircleRectangle(item.Charges, chargesWidth, currentPoint);
+                }
+                else
+                {
+                    DrawingUtils.CheckBoxRectangle(item.Charges, chargesWidth, currentPoint);
+                }
+
+                if (indendationLevel == 2)
+                {
+                    indendationLevel = 0;
+                    currentPoint.X = innerRegion.Left;
+                    currentPoint.Offset(0, DrawingUtils.DefaultLineHeight);
+                }
+                else
+                {
+                    indendationLevel += 1;
+                    currentPoint.X = innerRegion.Left + (thirdWidth * indendationLevel);
+                }
+            }
+        }
+
+
+        public ChargedAbilitySplit CreateAbilityDistributions(IEnumerable<ChargedAbility> abilities)
+        {
+            var singleLineAbilites = new List<ChargedAbility>();
+            var doubledUpAbilies = new List<ChargedAbility>();
+            var tripledUpAbilities = new List<ChargedAbility>();
+
+            foreach (var ability in abilities)
+            {
+                if (ability.Charges == 1 && ability.Frequent != Frequency.Sectional)
+                {
+                    tripledUpAbilities.Add(ability);
+                }
+                else if (ability.Charges > 7 || (ability.Frequent == Frequency.Sectional && ability.Charges > 2))
+                {
+                    singleLineAbilites.Add(ability);
+                }
+                else
+                {
+                    doubledUpAbilies.Add(ability);
+                }
+            }
+
+            // Move triple to double where it doesn't cost a line
+            while (tripledUpAbilities.Count() % 3 != 0)
+            {
+                doubledUpAbilies.Add(tripledUpAbilities.First());
+                tripledUpAbilities.RemoveAt(0);
+            }
+
+            return new ChargedAbilitySplit()
+            {
+                SingleLineAbilites = singleLineAbilites,
+                DoubledUpAbilies = doubledUpAbilies,
+                TripledUpAbilities = tripledUpAbilities
+            };
         }
     }
 }
